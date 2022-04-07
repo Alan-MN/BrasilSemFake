5a0a917efc1c5e17539f6126517e5c2f193d4a0a
alan <alann678n@gmail.com>
2022-04-07 15:34:02 -0300

    criação regras email e senha

--- BrasilSemFakeApp/lib/src/features/auth/data/repository/login_repository.dart
+++ BrasilSemFakeApp/lib/src/features/auth/data/repository/login_repository.dart
@@ -4,6 +4,7 @@ import '../../domain/repository/login_interface.dart';
 class LoginRepository implements ILogin {
   @override
   Future<User> login(User user) {
+    //final ;
     throw UnimplementedError();
   }
 }
--- BrasilSemFakeApp/lib/src/features/auth/domain/usecase/login_usecase.dart
+++ BrasilSemFakeApp/lib/src/features/auth/domain/usecase/login_usecase.dart
@@ -6,6 +6,20 @@ import '../repository/login_interface.dart';
 class LoginUseCase {
   final repository = Modular.get<ILogin>();
 
+  String? validateUserName(String username) {
+    if (username.isEmpty) {
+      return 'username_required'.i18n();
+    }
+    return null;
+  }
+
+  String? validatePassword(String password) {
+    if (password.isEmpty) {
+      return 'password_required'.i18n();
+    }
+    return null;
+  }
+
   Future<User> login(String username, String password) {
     return repository.login(User(username, password));
   }
