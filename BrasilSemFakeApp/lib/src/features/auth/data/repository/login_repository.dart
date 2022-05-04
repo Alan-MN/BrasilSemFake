f0c5da47190d3098adb6ad75d084b747b5df079f
DanLucena <danlucas1711@gmail.com>
2022-05-04 01:50:27 -0300

    Fixing url

--- BrasilSemFakeApp/lib/src/features/auth/data/repository/login_repository.dart
+++ BrasilSemFakeApp/lib/src/features/auth/data/repository/login_repository.dart
@@ -7,7 +7,7 @@ class LoginRepository implements ILogin {
   @override
   Future<User> login(User user) async{
     try {
-      var response = await Dio().post('http://189.6.32.161:8000/user/login', data: {"username": user.username, "password": user.password});
+      var response = await Dio().post('http://localhost:8000/user/login', data: {"username": user.username, "password": user.password});
       return response as Future<User>;
     } catch (e) {
       throw Exception(e);
